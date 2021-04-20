8b9d5d09e7c6b84c50cdd9ffcf6149a086f941d0
Yahir Oropeza <dev.orcj@medical-minds.net>
2021-04-20 16:23:22 -0500

    solution

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ let toggle = document.getElementById("toggle");
 
 function component_quote(){
     let rangePricin = range_pricing.value;
-     var multi = range_pricing.value * 25;
-    var color = 'linear-gradient(to right, hsl(174, 77%, 80%) ' + multi + '%, hsl(224, 65%, 95%) ' + multi + '%)';
+    let multiple = range_pricing.value * 25;
+    let color = 'linear-gradient(to right, hsl(174, 77%, 80%) ' + multiple + '%, hsl(224, 65%, 95%) ' + multiple + '%)';
     range_pricing.style.background = color;
 
     if(toggle.checked == false){
@@ -36,27 +36,27 @@ function component_quote(){
     else {
         type_bill.innerHTML = "/year";
         if(rangePricin == 0){
-            var valueForYear =  8*12 - (8 * 12)*0.25;
+            let valueForYear =  8*12 - (8 * 12)*0.25;
             price.innerHTML = "$" + valueForYear + ".00" ;
             views.innerHTML = "10K pageviews";
         }
         else if(rangePricin == 1){
-            var valueForYear =  12*12 - (12 * 12)*0.25;
+            let valueForYear =  12*12 - (12 * 12)*0.25;
             price.innerHTML = "$" + valueForYear + ".00" ;
             views.innerHTML = "50K pageviews";
         }
         else if(rangePricin == 2){
-            var valueForYear =  16*12 - (16 * 12)*0.25;
+            let valueForYear =  16*12 - (16 * 12)*0.25;
             price.innerHTML = "$" + valueForYear + ".00" ;
             views.innerHTML = "100K pageviews";
         }
         else if(rangePricin == 3){
-            var valueForYear =  24*12 - (24 * 12)*0.25;
+            let valueForYear =  24*12 - (24 * 12)*0.25;
             price.innerHTML = "$" + valueForYear + ".00" ;
             views.innerHTML = "500k pageviews";
         }
         else if(rangePricin == 4){
-            var valueForYear =  36*12 - (36 * 12)*0.25;
+            let valueForYear =  36*12 - (36 * 12)*0.25;
             price.innerHTML = "$" + valueForYear + ".00" ;
             views.innerHTML = "1M pageviews";
         }
